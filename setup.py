6f8013dba59c67197546829570de3afae72e1563
ryanreyn <ryanreyn@usc.edu>
2025-08-06 15:32:43 -0700

    Adding dynamic versioning to config files, including a pyproject.toml file for modernized deployment.

--- setup.py
+++ setup.py
@@ -2,7 +2,8 @@ from setuptools import setup, find_packages
 
 setup(
     name="carc_tutorial",
-    version="0.1.0",
+    usc_scm_version=True,
+    setup_requires=["setuptools_scm"]
     packages=find_packages(),
     include_package_data=True,
     package_data={
